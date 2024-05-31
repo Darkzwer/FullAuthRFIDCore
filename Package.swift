9c307627f2a7b7eb8eb5a38b34389e0b62241583
Darkzwer123 <137776351+Darkzwer123@users.noreply.github.com>
2024-05-31 17:00:36 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "FullAuthRFIDCore",
-            targets: ["FullAuthRFIDCore"]),
+            targets: ["FullAuthRFIDCoreTarget"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,11 +18,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "FullAuthRFIDCore",
-            dependencies: []),
-        .testTarget(
-            name: "FullAuthRFIDCoreTests",
-            dependencies: ["FullAuthRFIDCore"]),
+        .binaryTarget(name: "FullAuthRFIDCoreTarget", url: "https://pods.regulaforensics.com/FullAuthRFID/7.2.9114/DocumentReaderCore_fullauthrfid_7.2.9114.zip", checksum: "30a37ea342ac74e6b027fa81e3d98111fdd0d9192916dc72059c12b08fc3e479")
     ]
 )
